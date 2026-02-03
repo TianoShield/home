366cf5ec84cd7c4d172dfed4cfdc656543b2ca79
Neelesh Sai <rneeleshsai@gmail.com>
2026-02-03 12:02:46 -0700

    updated build method to default

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    //minify: 'terser', //using default esbuild
     rollupOptions: {
       output: {
         manualChunks: {
